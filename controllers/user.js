df83b4eab466f0338e32a7a85fca687898979006
dydgjs200 <dydgjs2016@naver.com>
2024-05-26 20:42:14 +0900

    rank 찾기 기능

--- controllers/user.js
+++ controllers/user.js
@@ -9,17 +9,26 @@ exports.getAllUsers = (req, res) => {
 
 exports.getUserInfo = async (req, res) => {
   try {
-    const { gameName } = encodeURIComponent(req.params);
+    const gameName = encodeURIComponent(req.params.gameName);
     const region = req.body.region;
     const tagLine = req.body.tagLine;
+
+    // gameName + tagLine을 이용한 puuid
     const accountUrl = `https://${region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}?api_key=${process.env.API_KEY}`;
+    const getPuuid = await axios.get(accountUrl);
+    const puuid = getPuuid.data.puuid;
 
-    const getPuuid = await axios.get(accountUrl).then((response) => {
-      console.log("response > ", response.data);
-    });
-    console.log(getPuuid); //why undefined?
+    // puuid를 이용한 summonerId
+    const summonerUrl = `https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}?api_key=${process.env.API_KEY}`;
+    const getSummonerId = await axios.get(summonerUrl);
+    const summonerId = getSummonerId.data.id; // summonerId = id
 
-    res.send(getPuuid);
+    // summonerId를 이용한 rank 찾기
+    const rankUrl = `https://kr.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${process.env.API_KEY}`;
+    const getRank = await axios.get(rankUrl);
+    const rankInfo = getRank.data;
+
+    res.send(rankInfo);
   } catch (err) {
     console.log("error > ", err);
     res.status(500).json({ message: "interval server error" });
