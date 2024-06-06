539aa80980ec918e9c850edb1ff3fcbc136fe1e6
I00jey <cyj5056@gmail.com>
2024-06-06 15:52:24 +0900

    user 테이블 속성 변경

---
+++ migrations/20240606064628-remove-column-from-table.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.removeColumn("users", "accountId");
+        await queryInterface.renameTable("users", "user");
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+    },
+};
