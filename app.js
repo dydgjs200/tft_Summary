5a0c6c5a339125c51898c404dbfff4b0dfe490be
dydgjs200 <dydgjs2016@naver.com>
2025-01-07 12:32:41 +0900

    views index.html

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const dotenv = require("dotenv");
 const sequelize = require("./models/index");
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,7 +20,15 @@ dotenv.config();
 const userRoutes = require("./routes/user/index");
 app.use("/user", userRoutes);
 
+// views import
+const path = require("path")
+app.use(express.static(path.join(__dirname, "views")));
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("서버 실행");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
